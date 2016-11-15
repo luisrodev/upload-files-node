cb96e1ccf05a1f8bb6e789d7f3cb6a8142ca735d
Luis A. Rubio <luirting@gmail.com>
2016-11-15 12:33:55 -0700

    Created assets & view folder, index page view only
    Created assets folder and move css, imgs, js & libs folder in.
    Crated view folder with index.jade file, works view only.

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "jade");
+
+app.use(express.static("assets"));
+
 app.get("/", function(req, res){
-  res.send("Working");
+  //res.send("Working");
+  res.render("index");
 });
 
 
